5eea91f8623df3943937067d49eb0d72ad3d78c0
WenConrad <conradwen@gmail.com>
2022-01-12 22:34:02 -0800

    add controller and route for admin categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,7 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    @products = Product.order(id: :desc).all
+  end
+end
