907e59e28f457e23a47cd627fb6c75f956ba2f6c
WenConrad <conradwen@gmail.com>
2022-02-02 16:18:11 -0800

    user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 3 }, confirmation: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "should save sucessfully" do
+      @user = User.new({:name => "name", :email => "email", :password => "password", :password_confirmation => "password"})
+      expect{@user.save!}.to_not raise_error()
+    end
+    it "should validate name" do
+      @user = User.new({:name => nil, :email => "email", :password => "password", :password_confirmation => "password"})
+      expect{@user.save!}.to raise_error()
+    end
+    describe "should validate email" do
+      it "should check email not nil" do
+        @user = User.new({:name => "name", :email => nil, :password => "password", :password_confirmation => "password"})
+        expect{@user.save!}.to raise_error()
+      end
+      it "should check if email exists" do
+        @user = User.new({:name => "name", :email => "email", :password => "password", :password_confirmation => "password"})
+        @user.save!
+        @user2 = User.new({:name => "name", :email => "EMAIL", :password => "password", :password_confirmation => "password"})
+        expect{@user2.save!}.to raise_error()
+      end
+    end
+    describe "should validate password" do
+      it "should check password exists" do
+        @user = User.new({:name => "name", :email => "email", :password => nil, :password_confirmation => nil})
+        expect{@user.save!}.to raise_error()
+      end
+      it "should check password matches" do
+        @user = User.new({:name => "name", :email => "email", :password => "password", :password_confirmation => "passnotword"})
+        expect{@user.save!}.to raise_error()
+      end
+      it "should check password length" do
+        @user = User.new({:name => "name", :email => "email", :password => "pw", :password_confirmation => "pw"})
+        expect{@user.save!}.to raise_error()
+      end
+    end
+  end
 end
